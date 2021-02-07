d928dc882e36f5bbf5fd16cc6889e314dfbd10f8
Erfan <57976517+erfanhss@users.noreply.github.com>
2021-02-07 11:43:49 -0500

    Update Gradient_Coding_Simple.py

--- Gradient_Coding_Simple.py
+++ Gradient_Coding_Simple.py
@@ -162,7 +162,7 @@ def master():
 def worker():
     model_under_operation = 0
     state = 0
-    seed_arr = np.random.RandomState(seed=rank).randint(0, 100000, size=num_slots)
+    seed_arr = np.random.RandomState(seed=rank+2).randint(0, 100000, size=num_slots)
     for slot in range(num_slots):
         # determine whether a node is straggler
         if state == 0:
@@ -225,17 +225,17 @@ rank = comm.Get_rank()
 x_train = np.reshape(x_train, (-1, 28, 28, 1)) / 255.
 x_test = np.reshape(x_test, (-1, 28, 28, 1)) / 255.
 batch_size_per_worker = 256
-alpha = 10
+alpha = 5
 tol = 0.9
-num_slots = 5000
+num_slots = 6000
 num_workers = 4
 epsilon = 2
-num_models = 22
+num_models = 12
 lr_list = np.linspace(0.01, 0.1, num_models)
 models = [Model(lr) for lr in lr_list]
 a = 0.05
-b = 0.1
-num_states = 2
+b = 0.7
+num_states = 4
 if rank == 0:
     master()
 else:
