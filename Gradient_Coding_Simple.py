d99bda29b71268ff27e85c410e9b583eece8c7a7
Erfan <57976517+erfanhss@users.noreply.github.com>
2021-05-17 17:36:39 -0400

    Update Gradient_Coding_Simple.py

--- Gradient_Coding_Simple.py
+++ Gradient_Coding_Simple.py
@@ -230,12 +230,12 @@ tol = 0.9
 num_slots = 6000
 num_workers = 4
 epsilon = 2
-num_models = 12
+num_models = 4
 lr_list = np.linspace(0.01, 0.1, num_models)
 models = [Model(lr) for lr in lr_list]
-a = 0.05
-b = 0.7
-num_states = 4
+a = 0.2
+b = 0.8
+num_states = 1
 if rank == 0:
     master()
 else:
